a3d68eab5e10dcf4d162bc1e70507a22c14978eb
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-06-23 20:21:13 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY);
+
+  return (
+    <h1>Hello world</h1>
+  )
+}
+
+export default App
